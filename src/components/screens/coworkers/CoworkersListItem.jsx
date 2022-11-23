8a75903b9e04fbf350a2868a72f4ecbe06093a05
Misha <xxxdaddy420@gmail.com>
2022-11-23 17:08:28 +0700

    fix status in adaptive

--- src/components/screens/coworkers/CoworkersListItem.jsx
+++ src/components/screens/coworkers/CoworkersListItem.jsx
@@ -1,7 +1,13 @@
-
+import useWindowDimensions from '../../../hooks/useWindowDimensions'
 import { Row, Col } from 'react-bootstrap'
 
 const CoworkersListItem = ({name, position, phone, birthday, place, status, quote}) => {
+    
+    const { width } = useWindowDimensions();
+    let statusClassName = "item__status";
+
+    status === "Online" ? statusClassName += " item__status-online" : status === "Offline" ? statusClassName += " item__status-offline" : statusClassName += " item__status-away"
+ 
     return (
         <li className="coworkers__list-item">
             <Row className="d-flex align-items-center">
@@ -10,7 +16,7 @@ const CoworkersListItem = ({name, position, phone, birthday, place, status, quot
                 <div className="item__name-wrapper">
                     <div  className="item__round">чт</div>
                     <div className="item__block">
-                        <span className="item__name">{name}</span>
+                        <span className={(width > 767 ? 'item__name' : statusClassName)}>{name}</span>
                         <span className="item__position">{position}</span>
                     </div>
                 </div>
@@ -33,7 +39,7 @@ const CoworkersListItem = ({name, position, phone, birthday, place, status, quot
            </Col>
             <Col xxl={1} xl={2} lg={2} md={4} sm={4} className="d-xxl-block d-xl-block d-lg-block d-md-block d-none">
             <div className="item">
-                <span className="item__status item__status-active">{status}</span>
+                <span className={statusClassName}>{status}</span>
             </div>
             </Col>
             <Col xxl={3} xl={0} className="d-xxl-block d-none">
@@ -42,7 +48,7 @@ const CoworkersListItem = ({name, position, phone, birthday, place, status, quot
             </div>
             </Col>
             <Col xxl={0} xl={2} lg={2} md={2} sm={4} xs={4}>
-                <div className="item__more">Подробнее</div>
+                <div className="item__more d-xxl-none d-xl-block"></div>
             </Col>
             </Row>
         </li>
@@ -50,29 +56,3 @@ const CoworkersListItem = ({name, position, phone, birthday, place, status, quot
 }
 
 export default CoworkersListItem;
-
-
-// <div className="item">
-// <div className="item__name-wrapper">
-//     <div  className="item__round">чт</div>
-//     <div className="item__block">
-//         <span className="item__name">{name}</span>
-//         <span className="item__position">{position}</span>
-//     </div>
-// </div>
-// </div>
-// <div className="item">
-// <a href="tel:+79233296003" className="item__phone">{phone}</a>
-// </div>
-// <div className="item">
-// <span className="item__birthday">{birthday}</span>
-// </div>
-// <div className="item">
-// <span className="item__place">{place}</span>
-// </div>
-// <div className="item">
-// <span className="item__status item__status-active">{status}</span>
-// </div>
-// <div className="item">
-// <div className="item__quote">{quote}</div>
-// </div>
